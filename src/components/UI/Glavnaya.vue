75cb75e208d3d1fc619117dc961f4378396e9d19
Iren0708 <sti4070802@mail.ru>
2025-05-19 22:11:23 +0300

    main

---
+++ src/components/UI/Glavnaya.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="sidebar-news" style="font-size: 15px">
+      <div class="text-news">
+        <div class="sidebar-news-block">
+          <img src="/src/assets/Home.svg" width="17" height="20" alt="Дом" />
+          <button href="" style="margin-left: 10px; padding: 0">
+            Последние новости
+          </button>
+        </div>
+      </div>
+      <div class="text-news">
+        <div class="sidebar-news-block">
+          <img src="/src/assets/Fire.svg" width="16" height="20" alt="Огонь" />
+          <button href="" style="margin-left: 10px; padding: 0">
+            Самые просматриваемые
+          </button>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <style scoped>
+  .sidebar-news {
+    overflow-y: auto;
+  }
+  .text-news {
+  }
+  .text-news:hover {
+    color: #1976d2;
+    background: #ddeaf8;
+  }
+  .sidebar-news-block {
+    padding: 8px 0 3px 5px;
+    display: flex;
+    flex-wrap: wrap;
+  }
+  </style>
+  
--- src/views/NewsCard.vue
+++ src/views/NewsCard.vue
@@ -1,89 +1,159 @@
-
 <template>
-<div class="conteiner">
-        <div class="zagolovok" >
-          <v-icon icon="mdi:mdi-arrow-left" style="width: 24px;height: 24px;"></v-icon>
-          <h2 class="nazvanie" style="font-family: Open Sans;font-weight: 600;font-size: 34px;line-height: 40px;letter-spacing: 0%;"
- >В обществе прошли мероприятия, посвящённые Дню Победы</h2>
-        </div>
-      <div class="oblastotobr">
-      <div class="img"></div>
-      <div>
-        <div class="data">16 мая 2024 12:00</div>
-        <div class="text">В обществе прошли мероприятия, посвящённые Дню Победы. В них приняли участие 156 работников.</div>
+  <div>
+    <div>
+      <h2 class="Zag">
+        <button class="btn" @click="openNews">
+          <v-icon style="width: 24px;height: 24px; padding: 0 !important; " 
+             icon="mdi: mdi-arrow-left"></v-icon></button>{{ Zag }} 
+      </h2>
+    </div>
+    <div style="padding-left: 20px;">
+      <div class="data">{{ data }}</div>
+      <div class="btn" style="padding-left: 0 !important;">
+        <button class="redact">
+          <v-icon
+            icon="mdi-pencil"
+            color="#1976D2"
+            width="18px"
+            height="18px"
+          ></v-icon
+          >Редактировать
+        </button>
+        <button class="snyat">
+          <v-icon
+            icon="mdi-close"
+            color="#1976D2"
+            width="14px"
+            height="14px"
+          ></v-icon>Снять с публикации
+        </button>
+        <button class="delete">
+          <v-icon
+            icon="mdi-delete" color="#FF5252" width="14px" height="18px"
+          ></v-icon>Удалить
+        </button>
+      </div>
+      <div class="im_nazv">
+        <div class="image"></div>
+        <div class="nazvanie" >{{ nazvanie }}</div>
+      </div>
+      <div class="text">
+        <div>{{ par1 }}</div>
+        <div>{{ par2 }}</div>
+        <div>{{ par3 }}</div>
+        <div>{{ par4 }}</div>
+        <div>{{ par5 }}</div>
+        <div>{{ par6 }}</div>
       </div>
     </div>
-</div>    
+  </div>
 </template>
+
 <style scoped>
-  .oblastotobr {
-    width: 1800px;
-    height: 353px;
-    display: flex;
-    flex-wrap: wrap;
-  }
-  .img {
-    background: #d9d9d9;
-    width: 627px;
-    height: 353px;
-  }
-  .data {
-    text-align: left;
-    padding-left: 20px;
-    font-size: 12px;
-    color: gray;
-    margin-bottom: 30px;
-    font-family: Open Sans;
-    width: 350px;
-  }
-  .text-for-news {
-    text-align: left;
-    padding-left: 20px;
-    margin-bottom: 15px;
-    font-weight: 600px;
-    font-size: 20px;
-    font-family: Open Sans;
-    width: 350px;
-  }
-  .text {
-    text-align: left;
-    padding-left: 20px;
-    margin-bottom: 50px;
-    font-family: Open Sans;
-    font-weight: 400px;
-    width: 350px;
-  }
-  .btn {
-    background: #1976d2;
-    border-radius: 5px;
-    color: white;
-    text-align: left;
-    padding-left: 20px;
-  }
-  .conteiner {
-    left: 0 !important;
-  }
-  
-  .zagolovok {
-    background: #f6f6f6;
-    width: 1611px;
-    height: 15%;
-    padding-top: 16px;
-    padding-right: 24px;
-    padding-left: 24px;
-    width: 100%;
-    height: 100%;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    padding: 56px 24px;
-    gap: 10px;
-  }
-  </style>
-  
-  <script>
+.Zag {
+  background: #f6f6f6;
+  width: 1611px;
+  height: 152px;
+  padding-top: 56px;
+  padding-bottom: 56px;
+  padding-right: 24px;
+  padding-left: 24px;
+  font-family: Open Sans;
+  width: 350px;
+  font-size: 34px;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  text-align: left;
+}
+.nazvanie {
+  display: flex;
+  flex-wrap: wrap;
+  width: 430px;
+  font-family: Open Sans;
+  font-weight: 600;
+  text-align: left;
+  padding-left: 20px;
+}
+.btn {
+  padding-left: 20px;
+  display: flex;
+  flex-wrap: wrap;
+}
+.data {
+  text-align: left;
+  padding-left: 20px;
+  font-size: 12px;
+  color: gray;
+  margin-top: 20px;
+  margin-bottom: 0px;
+  font-family: Open Sans;
+  width: 350px;
+}
+.image {
+  background: #d9d9d9;
+  width: 733px;
+  height: 429px;
+}
+.text {
+  display: flex;
+  flex-wrap: wrap;
+  width: 921px;
+  height: 375px;
+  text-align: justify;
+  padding-top: 10px;
+}
+.im_nazv {
+  margin-top: 20px;
+  display: flex;
+  flex-wrap: wrap;
+}
+.redact {
+  min-width: 193px;
+  height: 40px;
+  border: 1px solid #aaafb9;
+  border-radius: 5px 0px 0px 5px;
+  margin-top: 20px;
+  padding-bottom: 35px;
+}
+.snyat {
+  min-width: 193px;
+  height: 40px;
+  border: 1px solid #aaafb9;
+  border-radius: 0px 0px 0px 0px;
+  margin-top: 20px;
+  padding-bottom: 35px;
+}
+.delete {
+  min-width: 193px;
+  height: 40px;
+  border: 1px solid #aaafb9;
+  border-radius: 0px 5px 5px 0px;
+  margin-top: 20px;
+  padding-bottom: 35px;
+}
+</style>
+
+<script>
 export default {
     name: 'NewsCard',
-    
+    data() {
+    return {
+      Zag: 'В Обществе прошли мероприятия, посвящённые Дню Победы',
+      data: '16 мая 2024 12:00',
+      nazvanie: 'В Обществе прошли мероприятия, посвящённые Дню Победы. В них приняли участие 156 работников.',
+      par1: 'Собравшихся приветствовал генеральный директор СПО «Арктика» Юрий Душкин:',
+      par2: '– В мае 2005 года у центральной вахты нашего предприятия была открыта мемориальная стела, посвящённая памяти работников СПО «Арктика» – участников Великой Отечественной. На стеле в бронзе отлиты имена 138 воинов-победителей. Когда дети и молодые люди приходят на предприятие с экскурсиями, они обращают внимание на этот мемориальный комплекс. Замечательно, что мы сохраняем память о тех, кто ковал Победу: на фронте или в тылу, совершая трудовые подвиги, осуществляя ремонт судов и кораблей. Низкий им поклон... ',
+      par3: 'Председатель Совета ветеранов СПО «Арктика» Геннадий Подганин отметил, что всё меньше остаётся свидетелей событий Великой Отечественной войны. Уже нет в живых ни одного участника боевых действий – бывших работников СПО «Арктика». Сейчас в составе ветеранской организации 13 тружеников тыла, одна вдова фронтовика, двое жителей блокадного Ленинграда и более 380 человек, относящихся к категории «дети войны». Он подчеркнул, что задача поколения детей войны – как можно чаще встречаться со школьниками и студентами, чтобы рассказывать об уроках той войны.',
+      par4: 'По традиции руководители предприятия и ветераны возложили цветы к стеле у центральной вахты. А затем они проехали к памятному знаку Александру Дубинину и мемориальной доске Александру Паршинову на площади Ломоносова в Северодвинске. Ветераны почтили память бывших директоров, которые были участниками Великой Отечественной войны.',
+      par5: '8 мая СПО «Арктика» присоединилось к всероссийской акции «Георгиевская ленточка». У центральной вахты предприятия сотрудники отдела обучения и развития персонала раздавали всем желающим символ памяти о героизме, доблести и славе защитников Отечества.',
+      par6: 'Также 8 мая руководители СПО «Арктика» во главе с генеральным директором Юрием Душкиным приняли участие в городских мероприятиях Северодвинска, посвящённых памяти победителей. В полдень у знака «Ратному подвигу северодвинцев» состоялся торжественный митинг и возложение цветов к Вечному огню. В церемонии приняли участие представители руководства города и области, предприятий и общественных организаций Северодвинска, военнослужащие, почётные гости.',
+    };
+  },   
+  methods: {
+    openNews() {
+      this.$router.push({ name: 'News' });
+    },
+  }, 
 }
 </script>
